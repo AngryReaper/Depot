e8f1e499b2c7117183bdbe656f622d1462028304
AngryReaper <angryreaper@gmail.com>
2014-06-01 00:48:20 +0400

    new ath

--- test/integration/user_stories_test.rb
+++ test/integration/user_stories_test.rb
@@ -28,7 +28,7 @@ class UserStoriesTest < ActionDispatch::IntegrationTest
 				post_via_redirect "/orders",
 												order: { name:        "Dave Thomas",
 																address:      "123 The Street",
-																email:        "dave@example.com",
+																email:        "angryreaper@gmail.com",
 																pay_type:     "Check" }
 				assert_response :success
 				assert_template "index"
@@ -41,7 +41,7 @@ class UserStoriesTest < ActionDispatch::IntegrationTest
 
 				assert_equal "Dave Thomas",				order.name
 				assert_equal "123 The Street",		order.address
-				assert_equal "dave@example.com",	order.email
+				assert_equal "angryreaper@gmail.com",	order.email
 				assert_equal "Check",							order.pay_type
 
 				assert_equal 1, order.line_items.size
@@ -49,7 +49,7 @@ class UserStoriesTest < ActionDispatch::IntegrationTest
 				assert_equal ruby_book, line_item.product
 
 				mail = ActionMailer::Base.deliveries.last
-				assert_equal ["dave@example.com"], mail.to
+				assert_equal ["angryreaper@gmail.com"], mail.to
 				assert_equal 'Sam Ruby <depot@example.com>', mail[:from].value
 				assert_equal "Pragmatic Store Order Confirmation", mail.subject
 		end
