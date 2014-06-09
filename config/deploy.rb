d9c1ec2d635c3dc735ddb17e9db88b26b17f0f99
AngryReaper <angryreaper@gmail.com>
2014-06-09 22:34:29 +0400

    cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,36 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require 'bundler/capistrano'
+# be sure to change these
+set :user, 'username'
+set :domain, 'www.depot.com'
+set :application, 'depot'
+
+# adjust if you are using RVM, remove if you are not
+set :rvm_type, :user
+set :rvm_ruby_string, 'ruby-2.0.0-p451'
+require 'rvm/capistrano'
+
+# file paths
+set :repository, "#{user}@#{domain}:git/#{application}.git"
+set :deploy_to, "/var/www/#{application}"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, domain                          # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  domain, :primary => true # This is where Rails migrations will run
+
+set :deploy_via, :remote_cache
+set :scm, 'git'
+set :branch, 'master'
+set :scm_verbose, true
+set :use_sudo, false
+set :rails_env, :production
+
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
-
+default_run_options[:pty] = true
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
@@ -23,3 +42,20 @@ role :db,  "your slave db-server here"
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+namespace :deploy do
+    desc "cause Passenger to initiate a restart"
+    task :restart do
+        run "touch #{current_path}/tmp/restart.txt"
+    end
+
+    desc "reload the database with seed data"
+    task :seed do
+        run "cd #{current_path}; rake db:seed RAILS_ENV=#{rails_env}"
+    end
+end
+
+after "deploy:update_code", :bundle_install
+desc "install the necessary prerequisites"
+task :bundle_install, :roles => :app do
+    run "cd #{release_path} && bundle install"
+end
